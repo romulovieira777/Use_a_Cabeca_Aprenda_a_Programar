263f3194b0fecfb44f2d72e510528e1e04ab00df
Romulo Vieira <romulo.vieira777@gmail.com>
2024-01-12 10:12:03 -0300

    Funções e Abstração

---
+++ Capitulo_06_Funcoes_e_Abstracao/02_dog_v2.py
@@ -0,0 +1,16 @@
+print('Get those dogs ready')
+
+
+def bark(name, weight):
+    if weight > 20:
+        print(name, 'says WOOF WOOF')
+    else:
+        print(name, 'says woof woof')
+
+
+bark('Codie', 40)
+bark('Sparky', 9)
+bark('Jackson', 12)
+bark('Fido', 65)
+
+print("Okay we're all done")
