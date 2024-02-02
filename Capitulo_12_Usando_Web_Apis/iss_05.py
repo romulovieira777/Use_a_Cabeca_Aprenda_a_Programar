16a19bb7a8f087b0c177b8f6d8c9e39d537af962
Romulo Vieira <romulo.vieira777@gmail.com>
2024-02-02 09:47:56 -0300

    Usando Web Apis

---
+++ Capitulo_12_Usando_Web_Apis/iss_05.py
@@ -0,0 +1,34 @@
+import json
+import requests
+import turtle
+
+
+def move_iss(lat, long):
+    global iss
+
+    iss.penup()
+    iss.goto(long, lat)
+    iss.pendown()
+
+
+screen = turtle.Screen()
+screen.setup(1000, 500)
+screen.bgpic('earth.gif')
+screen.setworldcoordinates(-180, -90, 180, 90)
+
+iss = turtle.Turtle()
+turtle.register_shape('iss.gif')
+iss.shape('iss.gif')
+
+url = 'http://api.open-notify.org/iss-now.json'
+
+reposnse = requests.get(url)
+
+if reposnse.status_code == 200:
+    response_dictionary = json.loads(reposnse.text)
+    position = response_dictionary['iss_position']
+    lat = float(position['latitude'])
+    long = float(position['longitude'])
+    move_iss(lat, long)
+else:
+    print('Houston, we have a problem:', reposnse.status_code)
---
+++ Capitulo_12_Usando_Web_Apis/iss_06.py
@@ -0,0 +1,51 @@
+import json
+import requests
+import turtle
+
+
+iss = turtle.Turtle()
+
+
+def setup(window):
+    global iss
+
+    window.setup(1000, 500)
+    window.bgpic('earth.gif')
+    window.setworldcoordinates(-180, -90, 180, 90)
+    turtle.register_shape('iss.gif')
+    iss.shape('iss.gif')
+
+
+def move_iss(lat, long):
+    global iss
+
+    iss.penup()
+    iss.goto(long, lat)
+    iss.pendown()
+
+
+def track_iss():
+    url = 'http://api.open-notify.org/iss-now.json'
+    reposnse = requests.get(url)
+
+    if reposnse.status_code == 200:
+        response_dictionary = json.loads(reposnse.text)
+        position = response_dictionary['iss_position']
+        lat = float(position['latitude'])
+        long = float(position['longitude'])
+        move_iss(lat, long)
+    else:
+        print('Houston, we have a problem:', reposnse.status_code)
+
+
+def main():
+    global iss
+
+    screen = turtle.Screen()
+    setup(screen)
+    track_iss()
+
+
+if __name__ == '__main__':
+    main()
+    turtle.mainloop()
