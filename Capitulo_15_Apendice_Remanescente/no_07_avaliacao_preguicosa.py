0e26be98608b176ef90c5d50b9a8c77cd2334926
Romulo Vieira <romulo.vieira777@gmail.com>
2024-02-14 12:00:31 -0300

    Apêndice: Remanescente

--- Capitulo_15_Apendice_Remanescente/no_07_avaliacao_preguicosa.py
+++ Capitulo_15_Apendice_Remanescente/no_07_avaliacao_preguicosa.py
@@ -1,3 +1,4 @@
+# Solution 01
 def nth_prime(n):
     count = 0
 
@@ -37,3 +38,42 @@ def list_of_primes():
         next_prime = get_prime(next)
         next = next + 1
         yield next_prime
+
+
+# Solution 02
+def nth_prime(n):
+    count = 0
+
+    for prime in list_of_primes():
+        count += 1
+        if count == n:
+            return prime
+
+
+def list_of_primes():
+    next = 1
+    while True:
+        next_prime = get_prime(next)
+        next += 1
+        yield next_prime
+
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def get_prime(n):
+    count = 0
+    num = 1
+    while count < n:
+        num += 1
+        if is_prime(num):
+            count += 1
+    return num
+
+
